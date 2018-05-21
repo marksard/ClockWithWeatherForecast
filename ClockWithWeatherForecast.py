5cb23a2a99ab27c4b59f288139bf6af26334777b
marksard <marksard@rocketmail.com>
2018-05-21 11:20:48 +0900

    [update] refactoring

--- ClockWithWeatherForecast.py
+++ ClockWithWeatherForecast.py
@@ -19,10 +19,7 @@ if USE_BME == True:
 class QCustomLabel(QLabel):
     def __init__(self, text):
         super(QCustomLabel, self).__init__(text)
-
         self.font = QFont('Source Han Code JP M', 11)
-        # self.font = QFont('Digital-7', 11)
-
         self.setFont(self.font)
         self.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignHCenter)
         self.setContentsMargins(-2, -2, -2, -2)
@@ -40,51 +37,78 @@ class QCustomLabel(QLabel):
             baseSize = height
         else:
             baseSize = width
-            
+
         self.font.setPixelSize(baseSize * self.fontScale)
         self.setFont(self.font)
 
+
 class ClockDisplay:
+    WEATHER_ICON_DAY = {
+        'clear sky': '☀',
+        'few clouds': '☀☁',
+        'scattered clouds': '☀☁',
+        'broken clouds': '☁',
+        'overcast clouds': '☁',
+        'light rain': '☂',
+        'moderate rain': '☔',
+        'heavy intensity rain': '☔',
+        'thunderstorm with light rain': '⚡☂',
+        'thunderstorm with rain': '⚡☔',
+        'thunderstorm with heavy rain': '⚡☔',
+        'light snow': '☃',
+        'snow': '☃',
+        'heavy snow': '☃',
+    }
+
+    WEATHER_ICON_NIGHT = {
+        'clear sky': '☽',
+        'few clouds': '☽☁',
+        'scattered clouds': '☽☁',
+        'broken clouds': '☁',
+        'overcast clouds': '☁',
+        'light rain': '☂',
+        'moderate rain': '☔',
+        'heavy intensity rain': '☔',
+        'thunderstorm with light rain': '⚡☂',
+        'thunderstorm with rain': '⚡☔',
+        'thunderstorm with heavy rain': '⚡☔',
+        'light snow': '☃',
+        'snow': '☃',
+        'heavy snow': '☃',
+    }
+
     def __init__(self, app, window):
-        self.WEATHER_ICON = {
-            'clear sky': '☀',
-            'scattered clouds': '☀',
-            'few clouds': '☀>☁',
-            'overcast clouds': '☁',
-            'broken clouds': '☀',
-            'light rain': '☂',
-            'moderate rain': '☔',
-            'heavy intensity rain': '☔',
-            }
-        self.app = app
-        self.window = window
-        self.date = QCustomLabel('')
-        self.times = []
-        self.forecastTimes = []
-        self.forecastTimesUnit = QCustomLabel('')
-        self.forecastWeathers = []
-        self.forecastWeathersUnit = QCustomLabel('')
-        self.forecastTemps = []
-        self.forecastTempssUnit = QCustomLabel('')
-        self.forecastRains = []
-        self.forecastRainsUnit = QCustomLabel('')
-        self.temp = QCustomLabel('')
-        self.tempUnit = QCustomLabel('')
-        self.hum = QCustomLabel('')
-        self.humUnit = QCustomLabel('')
-        self.pres = QCustomLabel('')
-        self.presUnit = QCustomLabel('')
-        self.sec1_count = 1
-        self.sec10_count = 1
-        self.sec60_count = 1
-        self.halfhour_count = 0
-        self.halfhour_count2 = 0
-        self.fullMode = False
-        self.bme = object
+        self._app = app
+        self._window = window
+        self._labelDate = QCustomLabel('2018/5/16 (水)')
+        self._labelTimes = []
+        self._labelForecastTimes = []
+        self._labelForecastWeathers = []
+        self._labelForecastTemps = []
+        self._labelForecastRains = []
+        self._labelTemperature = QCustomLabel('22')
+        self._labelHumidity = QCustomLabel('38')
+        self._labelPressure = QCustomLabel('1011')
+
+        self._labelForecastTimesUnit = QCustomLabel('時')
+        self._labelForecastWeathersUnit = QCustomLabel('天気')
+        self._labelForecastTempsUnit = QCustomLabel('℃')
+        self._labelForecastRainsUnit = QCustomLabel('mm')
+        self._labelTemperatureUnit = QCustomLabel('℃')
+        self._labelHumidityUnit = QCustomLabel('%')
+        self._labelPressureUnit = QCustomLabel('hPa')
+
+        self._1SecCount = 1
+        self._10SecCount = 1
+        self._60SecCount = 1
+        self._halfHourCount1 = 0
+        self._halfHourCount2 = 0
+        self._fullMode = False
+        self._bme = object
 
         if USE_BME == True:
-            self.bme = bme280()
-            self.bme.initialize()
+            self._bme = bme280()
+            self._bme.initialize()
 
     def initializeDisplayItems(self):
         initTimes = ['23', ':', '45', '01']
@@ -93,133 +117,122 @@ class ClockDisplay:
         initForecastTemps = ['16', '12', '10', '12', '16', '20', '20']
         initForecastRains = ['0', '0', '0', '0', '2', '2', '6']
 
-        self.date.setText('2018/5/16 (水)')
-        self.date.setFontScale(1.1)
+        self._labelDate.setFontScale(1.1)
 
         for i in range(0, 4):
-            self.times.append(QCustomLabel(''))
-            self.times[-1].setText(initTimes[i])
-            self.times[-1].setFontScale(1.1)
+            self._labelTimes.append(QCustomLabel(''))
+            self._labelTimes[-1].setText(initTimes[i])
+            self._labelTimes[-1].setFontScale(1.1)
 
-        self.times[3].setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom)
+        self._labelTimes[3].setAlignment(
+            QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom)
 
         for i in range(0, 7):
-            self.forecastTimes.append(QCustomLabel(''))
-            self.forecastTimes[-1].setText(initForecastTimes[i])
-            self.forecastTimes[-1].setFontScale(0.5)
+            self._labelForecastTimes.append(QCustomLabel(initForecastTimes[i]))
+            self._labelForecastWeathers.append(
+                QCustomLabel(initForecastWeathers[i]))
+            self._labelForecastTemps.append(QCustomLabel(initForecastTemps[i]))
+            self._labelForecastRains.append(QCustomLabel(initForecastRains[i]))
+            self._labelForecastTimes[-1].setFontScale(0.5)
+            self._labelForecastTemps[-1].setFontScale(0.9)
+            self._labelForecastRains[-1].setFontScale(0.8)
 
-            self.forecastWeathers.append(QCustomLabel(''))
-            self.forecastWeathers[-1].setText(initForecastWeathers[i])
-
-            self.forecastTemps.append(QCustomLabel(str(i)))
-            self.forecastTemps[-1].setText(initForecastTemps[i])
-            self.forecastTemps[-1].setFontScale(0.9)
-
-            self.forecastRains.append(QCustomLabel(''))
-            self.forecastRains[-1].setText(initForecastRains[i])
-            self.forecastRains[-1].setFontScale(0.8)
-
-        self.forecastTimesUnit.setText('時')
-        self.forecastTimesUnit.setFontScale(0.8)
-        self.forecastWeathersUnit.setText('天気')
-        self.forecastWeathersUnit.setFontScale(0.8)
-        self.forecastTempssUnit.setText('℃')
-        self.forecastTempssUnit.setFontScale(0.8)
-        self.forecastRainsUnit.setText('mm')
-        self.forecastRainsUnit.setFontScale(0.8)
-
-        self.temp.setText('22')
-
-        self.tempUnit.setText('℃')
-
-        self.hum.setText('38')
-
-        self.humUnit.setText('%')
-
-        self.pres.setText('1011')
-
-        self.presUnit.setText('hPa')
+        self._labelForecastTimesUnit.setFontScale(0.8)
+        self._labelForecastWeathersUnit.setFontScale(0.8)
+        self._labelForecastTempsUnit.setFontScale(0.8)
+        self._labelForecastRainsUnit.setFontScale(0.8)
 
     def initializeDisplayLayout(self, layout):
         # addWidget(obj, row-pos, col-pos, row-span, col-span)
-        layout.addWidget(self.date, 0, 0, 1, 15)
+        layout.addWidget(self._labelDate, 0, 0, 1, 15)
 
-        layout.addWidget(self.times[0], 1, 0, 3, 6)
-        layout.addWidget(self.times[1], 1, 6, 3, 2)
-        layout.addWidget(self.times[2], 1, 8, 3, 6)
-        layout.addWidget(self.times[3], 1, 14, 3, 1)
+        layout.addWidget(self._labelTimes[0], 1, 0, 3, 6)
+        layout.addWidget(self._labelTimes[1], 1, 6, 3, 2)
+        layout.addWidget(self._labelTimes[2], 1, 8, 3, 6)
+        layout.addWidget(self._labelTimes[3], 1, 14, 3, 1)
 
         for i in range(0, 7):
-            layout.addWidget(self.forecastTimes[i], 4, i * 2, 1, 2)
-            layout.addWidget(self.forecastWeathers[i], 5, i * 2, 2, 2)
-            layout.addWidget(self.forecastTemps[i], 7, i * 2, 1, 2)
-            layout.addWidget(self.forecastRains[i], 8, i * 2, 1, 2)
+            layout.addWidget(self._labelForecastTimes[i], 4, i * 2, 1, 2)
+            layout.addWidget(self._labelForecastWeathers[i], 5, i * 2, 2, 2)
+            layout.addWidget(self._labelForecastTemps[i], 7, i * 2, 1, 2)
+            layout.addWidget(self._labelForecastRains[i], 8, i * 2, 1, 2)
 
-        layout.addWidget(self.forecastTimesUnit, 4, 14)
-        layout.addWidget(self.forecastWeathersUnit, 5, 14, 2, 1)
-        layout.addWidget(self.forecastTempssUnit, 7, 14)
-        layout.addWidget(self.forecastRainsUnit, 8, 14)
+        layout.addWidget(self._labelForecastTimesUnit, 4, 14)
+        layout.addWidget(self._labelForecastWeathersUnit, 5, 14, 2, 1)
+        layout.addWidget(self._labelForecastTempsUnit, 7, 14)
+        layout.addWidget(self._labelForecastRainsUnit, 8, 14)
 
-        layout.addWidget(self.temp, 1, 15, 2, 2)
-        layout.addWidget(self.tempUnit, 1, 17, 2, 1)
-        layout.addWidget(self.hum, 3, 15, 2, 2)
-        layout.addWidget(self.humUnit, 3, 17, 2, 1)
-        layout.addWidget(self.pres, 5, 15, 2, 2)
-        layout.addWidget(self.presUnit, 5, 17, 2, 1)
+        layout.addWidget(self._labelTemperature, 1, 15, 2, 2)
+        layout.addWidget(self._labelTemperatureUnit, 1, 17, 2, 1)
+        layout.addWidget(self._labelHumidity, 3, 15, 2, 2)
+        layout.addWidget(self._labelHumidityUnit, 3, 17, 2, 1)
+        layout.addWidget(self._labelPressure, 5, 15, 2, 2)
+        layout.addWidget(self._labelPressureUnit, 5, 17, 2, 1)
 
-        screenChangeButton = QPushButton() 
+        # Full screen change button
+        screenChangeButton = QPushButton()
         screenChangeButton.clicked.connect(self.changeScreenMode)
-        # fill empty grid
         layout.addWidget(screenChangeButton, 0, 15, 1, 3)
+
+        # Fill empty grid
         layout.addWidget(QLabel(), 7, 15, 2, 3)
 
-    # del setViewMode(self):
-
     def changeScreenMode(self):
-        if self.fullMode == True:
-            self.fullMode = False
+        if self._fullMode == True:
+            self._fullMode = False
             self.window.showNormal()
         else:
-            self.fullMode = True
+            self._fullMode = True
             self.window.showFullScreen()
 
     def setNightMode(self):
         styleNight = 'QWidget{background-color:#407b8e72;} QLabel, QPushButton{color:#DCF7C9; background-color:#262626;}'
-        app.setStyleSheet(styleNight)
+        self._app.setStyleSheet(styleNight)
 
     def setDayMode(self):
         styleDay = 'QWidget{background-color:#7b8e72;} QLabel, QPushButton{color:#262626; background-color:#F3F9F1;}'
-        app.setStyleSheet(styleDay)
+        self._app.setStyleSheet(styleDay)
 
     def updateWeather(self):
         weathers = weatherinfo.getWeatherForecast()
         for i in range(0, 7):
-            self.forecastTimes[i].setNum(weathers[i][0].hour)
-            if weathers[i][2] in self.WEATHER_ICON:
-                self.forecastWeathers[i].setText(self.WEATHER_ICON[weathers[i][2]])
-                self.forecastWeathers[i].resizeEvent(NotImplementedError)
-            self.forecastTemps[i].setText('{:.0f}'.format(weathers[i][3]))
-            self.forecastRains[i].setText('{:.0f}'.format(weathers[i][4]))
+            if len(weathers) > i:
+                hour = weathers[i][0].hour
+                icons = ClockDisplay.WEATHER_ICON_NIGHT
+                if hour >= 6 and hour <= 15:
+                    icons = ClockDisplay.WEATHER_ICON_DAY
+                self._labelForecastTimes[i].setNum(hour)
+                if weathers[i][1] in icons:
+                    self._labelForecastWeathers[i].setText(
+                        icons[weathers[i][1]])
+                    self._labelForecastWeathers[i].resizeEvent(None)
+                else:
+                    self._labelForecastWeathers[i].setText('-')
+                    self._labelForecastWeathers[i].resizeEvent(None)
+                    
+                self._labelForecastTemps[i].setText(
+                    '{:.0f}'.format(weathers[i][2]))
+                self._labelForecastRains[i].setText(
+                    '{:.0f}'.format(weathers[i][3]))
 
     def onTimer(self):
         now = datetime.datetime.today()
+        self._1SecCount -= 1
+        if self._1SecCount == 0:
+            self._1SecCount = 5     # 200ms timer / 5 count = 1sec
+            self._10SecCount -= 1
+            self._60SecCount -= 1
 
-        self.sec1_count -= 1
-        if self.sec1_count == 0:
-            self.sec1_count = 5
-            self.sec10_count -= 1
-            self.sec60_count -= 1
+            self._labelDate.setText(now.strftime('%Y/%m/%d %a'))
+            self._labelTimes[0].setText(now.strftime('%H'))
+            self._labelTimes[2].setText(now.strftime('%M'))
+            self._labelTimes[3].setText(now.strftime('%S'))
 
-            self.date.setText(now.strftime('%Y/%m/%d %a'))
-            self.times[0].setText(now.strftime('%H'))
-            self.times[2].setText(now.strftime('%M'))
-            self.times[3].setText(now.strftime('%S'))
+            if (now.minute == 25 or now.minute == 55) and self._60SecCount == 0:
+                self._halfHourCount1 -= 1
 
-            if (now.minute == 25 or now.minute == 55) and self.sec60_count == 0:
-                self.halfhour_count -= 1
-
-            if (now.minute == 30 or now.minute == 0) and self.sec60_count == 0:
-                self.halfhour_count2 -= 1
+            if (now.minute == 30 or now.minute == 0) and self._60SecCount == 0:
+                self._halfHourCount2 -= 1
 
             if now.hour == 6 and now.minute == 0 and now.second == 0:
                 self.setDayMode()
@@ -227,33 +240,32 @@ class ClockDisplay:
             if now.hour == 18 and now.minute == 0 and now.second == 0:
                 self.setNightMode()
 
-        if self.sec10_count == 0:
-            self.sec10_count = 10
+        if self._10SecCount == 0:
+            self._10SecCount = 10
             if USE_BME == True:
-                bmeStatuses = self.bme.getStatus()
-                self.temp.setText(bmeStatuses[0])
-                self.hum.setText(bmeStatuses[1])
-                self.pres.setText(bmeStatuses[2])
+                bmeStatuses = self._bme.getStatus()
+                self._labelTemperature.setText(bmeStatuses[0])
+                self._labelHumidity.setText(bmeStatuses[1])
+                self._labelPressure.setText(bmeStatuses[2])
 
             # if sec60_count == 0:
             #     sec60_count = 60
             #     update_ntp_status_thread()
 
-            if self.halfhour_count <= 0:
-                self.halfhour_count = 1
+            if self._halfHourCount1 <= 0:
+                self._halfHourCount1 = 1
                 # update_speedtest_thread()
                 self.updateWeather()
 
-            # if halfhour_count2 == 0:
-            #     halfhour_count2 = 1
+            # if _halfHourCount2 == 0:
+            #     _halfHourCount2 = 1
             #     update_write_csv()
 
-
         # now = datetime.datetime.today()
-        # self.date.setText(now.strftime('%Y/%m/%d %a'))
-        # self.times[0].setText(now.strftime('%H'))
-        # self.times[2].setText(now.strftime('%M'))
-        # self.times[3].setText(now.strftime('%S'))
+        # self._labelDate.setText(now.strftime('%Y/%m/%d %a'))
+        # self._labelTimes[0].setText(now.strftime('%H'))
+        # self._labelTimes[2].setText(now.strftime('%M'))
+        # self._labelTimes[3].setText(now.strftime('%S'))
 
         # if now.hour >= 18 and viewMode == False:
         #     self.viewMode = True
@@ -261,7 +273,6 @@ class ClockDisplay:
         # elif now.hour >= 6 and view == True:
         #     self.viewMode = False
         #     setViewMode(False)
-
 
 
 if __name__ == '__main__':
@@ -276,19 +287,17 @@ if __name__ == '__main__':
     dispItems.initializeDisplayItems()
     dispItems.initializeDisplayLayout(layout)
 
-    now = datetime.datetime.today()
+    timer = QTimer()
+    timer.timeout.connect(dispItems.onTimer)
+    timer.start(200)
 
+    now = datetime.datetime.today()
     if now.hour >= 18:
         dispItems.setNightMode()
     elif now.hour >= 6:
         dispItems.setDayMode()
 
-    timer = QTimer()
-    timer.timeout.connect(dispItems.onTimer)
-    timer.start(200)
-
     window.setLayout(layout)
-
-    window.resize(800, 480)
+    window.resize(300, 200)
     window.show()
     sys.exit(app.exec_())
--- weatherinfo.py
+++ weatherinfo.py
@@ -11,28 +11,35 @@ from pytz import timezone
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv(join(dirname(__file__), '.env'))
 
 API_KEY = os.environ.get("API_KEY")
 API_URL = "http://api.openweathermap.org/data/2.5/forecast?zip={zip}&units=metric&APPID={key}"
 
+
 def getWeatherForecast():
-    url = API_URL.format(zip = '537-0003,JP', key = API_KEY)
+    url = API_URL.format(zip='537-0003,JP', key=API_KEY)
     response = requests.get(url)
-    data = json.loads(response.text)
+    forecastData = json.loads(response.text)
 
     result = []
-    for entry in data['list']:
-        w_datetime = timezone('Asia/Tokyo').localize(datetime.datetime.fromtimestamp(entry['dt']))
-        w_weather = entry['weather'][0]['main']
-        w_weather_desc = entry['weather'][0]['description']
-        w_temp = entry['main']['temp']
-        w_rain = 0
-        if 'rain' in entry and '3h' in entry['rain']:
-            w_rain = entry['rain']['3h']
-        result.append([w_datetime, w_weather, w_weather_desc, w_temp, w_rain])
-        print('日時:{0} 天気:{1}({4}) 気温:{2} 雨量(mm):{3}'.format(w_datetime, w_weather, w_temp, w_rain, w_weather_desc))
+    if not ('list' in forecastData):
+        result.append([datetime.datetime.today(), 'clear sky', 40.2, 0.0])
+        return result
+
+    for item in forecastData['list']:
+        forecastDatetime = timezone(
+            'Asia/Tokyo').localize(datetime.datetime.fromtimestamp(item['dt']))
+        weatherDescription = item['weather'][0]['description']
+        temperature = item['main']['temp']
+        rainfall = 0
+        if 'rain' in item and '3h' in item['rain']:
+            rainfall = item['rain']['3h']
+        result.append(
+            [forecastDatetime, weatherDescription, temperature, rainfall])
+        print('日時:{0} 天気:{1} 気温(℃):{2} 雨量(mm):{3}'.format(
+            forecastDatetime, weatherDescription, temperature, rainfall))
 
     return result
 
+# getWeatherForecast()
